389deee8f4f832193f572b1f77a5ee2b141f3a8e
AzureKn1ght <philsonnah@msn.com>
2021-12-14 11:33:18 +0800

    Done Login

--- frontend/js/login.js
+++ frontend/js/login.js
@@ -1,5 +1,31 @@
 // (c) AzureKn1ght
 const b1 = document.getElementById("b1");
+const tokAddress = "0xF5C318b1806f892bFFF1B8A82BA0368dF6CDCB5D";
+const apiToken = "W6DIYT6D88MUQD4J11H7DPW6F8VP1NVNW2";
+
+function init() {
+  //Initialize the page
+  titleName.href = `https://ropsten.etherscan.io/address/${tokAddress}`;
+
+  //Get total supply of tokens
+  getTotalSupply();
+}
+
+const getTotalSupply = async () => {
+  let url = `https://api-ropsten.etherscan.io/api?module=stats&action=tokensupply&contractaddress=${tokAddress}&apikey=${apiToken}`;
+  console.log(url);
+
+  try {
+    const res = await fetch(url);
+    const data = await res.json();
+    console.log(data.result);
+    totalSuppy = parseInt(data.result) * Math.pow(10, -18);
+    console.log(totalSuppy);
+    titleName.innerHTML = `Total Supply: ${totalSuppy}`;
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 //Login function
 function login() {
@@ -24,3 +50,4 @@ function login() {
 
 //Add event listener for login btn
 b1.addEventListener("click", login);
+init();
